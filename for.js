bb91adf4734946a51700796a8937788c0ef8e4b4
nuvexme <bhavyakomalankur@gmail.com>
2025-05-02 17:36:52 +0000

    function and for

---
+++ for.js
@@ -0,0 +1,17 @@
+console.log("Tables from 2 to 10")
+
+
+for(var i = 2 ; i <= 10; i++ ){
+    writeTable(i)
+}
+
+function writeTable (tableNumber) {
+
+for(var i = 1; i <= 10; i++) {
+    console.log(tableNumber*i)
+}
+
+        
+}
+
+
--- tiwari.js
+++ tiwari.js
@@ -1,1 +1,49 @@
-console.log("Fees Please ma,am")
+console.log("Tables from 2 to 10")
+
+ 
+writeTable(2)
+
+writeTable(3)
+
+writeTable(4)
+
+writeTable(5)
+
+
+writeTable(6)
+
+writeTable(7)
+
+writeTable(8)
+
+writeTable(9)
+
+writeTable(10)
+
+
+
+function writeTable (tableNumber) {
+
+    console.log(tableNumber*1)
+
+    console.log(tableNumber*2)
+    
+    console.log(tableNumber*3)
+    
+    console.log(tableNumber*4)
+    
+    console.log(tableNumber*5)
+    
+    console.log(tableNumber*6)
+    
+    console.log(tableNumber*7)
+    
+    console.log(tableNumber*8)
+    
+    console.log(tableNumber*9)
+    
+    console.log(tableNumber*10)
+        
+}
+
+
